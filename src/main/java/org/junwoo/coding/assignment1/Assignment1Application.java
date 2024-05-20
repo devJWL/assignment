259575091ff0276316184629a073950c62f59bdc
devJWL <jwlee4450@gmail.com>
2024-05-20 16:58:17 +0900

    ✨ JpaConfiguration을 이용하여 JPAQueryFactory 빈 등록 및 @EnableJpaAuditing 이전

--- src/main/java/org/junwoo/coding/assignment1/Assignment1Application.java
+++ src/main/java/org/junwoo/coding/assignment1/Assignment1Application.java
@@ -2,9 +2,7 @@ package org.junwoo.coding.assignment1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class Assignment1Application {
 
